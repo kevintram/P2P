6ef95e5e4f4d120988d34a489113d0980860d5fd
Tyler <tylerlack13@gmail.com>
2022-10-17 19:28:52 -0400

    latest

--- src/messages/BitField.java
+++ src/messages/BitField.java
@@ -5,10 +5,16 @@ import java.io.IOException;
 
 import static messages.Util.intToByteArr;
 
+/*TODO: Tyler
+*  Compare bitfields, determine which has what you dont
+*  update bitfields based on new data
+* */
+
 public class BitField {
     public byte[] bitfield;
     int size;
     public BitField(byte[] bytes, int size) {
+        //parses the bytestream sent in, and creates a bitfield in format of 8 bits, space, 8 more bits
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         for(int i = 0; i < size; i++){
             baos.write(bytes[i]);
