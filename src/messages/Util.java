8a69c08c4969056ed83f84cdec45d12082486126
Tyler <tylerlack13@gmail.com>
2022-10-21 07:45:19 -0400

    made logging util

--- src/messages/Util.java
+++ src/messages/Util.java
@@ -1,6 +1,8 @@
 package messages;
 
 import java.nio.ByteBuffer;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Util {
     public static int byteArrToInt(byte[] bytes) {
@@ -12,5 +14,110 @@ public class Util {
 
     public static byte[] intToByteArr(int i ) {
         return ByteBuffer.allocate(4).putInt(i).array();
+    }
+
+
+    /**
+     * Log function for establishing TCP connection to peer
+     * @param hostID ID of the local peer
+     * @param connectionID ID of the peer being connected to
+     */
+    public static void logMakeConnection(int hostID, int connectionID){
+        System.out.format("[%s]: Peer [%d] makes a connection to Peer[%d]%n", LocalDateTime.now().toString(), hostID, connectionID);
+    }
+
+    /**
+     * Log function for when a peer connects to local
+     * @param hostID ID of the local peer
+     * @param connectionID ID of the peer being connected to
+     */
+    public static void logConnectionEstablished(int hostID, int connectionID){
+        System.out.format("[%s]: Peer [%d] is connected from Peer[%d]%n", LocalDateTime.now().toString(), hostID, connectionID);
+    }
+
+    /**
+     * @param hostID ID of local peer
+     * @param peerIDs Array of Peer ID's for the new local  neighbors
+     */
+    public static void logChangeNeighbors(int hostID, int[] peerIDs){
+        System.out.format("[%s]: Peer [%d] has the preferred neighbors [", LocalDateTime.now().toString(), hostID);
+        for(int id : peerIDs){
+            System.out.format("%d",id);
+        }
+        System.out.format("]%n");
+    }
+
+    /**
+     * Logs change of optimistic preferred neighbor
+     * @param hostID id of local peer
+     * @param peerID id of new Optimistic neighbor
+     */
+    public static void logOptChangeNeighbor(int hostID, int peerID){
+        System.out.format("[%s]: Peer [%d] has the optimistically unchoked neighbor [%d]%n", LocalDateTime.now().toString(), hostID, peerID);
+    }
+
+    /**
+     * logs when local peer is unchoked
+     * @param p1ID Peer being unchoked
+     * @param p2ID Peer unchoking
+     */
+    public static void logUnchoke(int p1ID, int p2ID){
+        System.out.format("[%s]: Peer [%d] is unchoked by [%d]%n", LocalDateTime.now().toString(), p1ID, p2ID);
+    }
+
+    /**
+     * log for when a peer chokes another peer
+     * @param p1ID peer being choked
+     * @param p2ID peer doing the choking
+     */
+    public static void logChoke(int p1ID, int p2ID){
+        System.out.format("[%s]: Peer [%d] is choked by [%d]%n", LocalDateTime.now().toString(), p1ID, p2ID);
+    }
+
+    /**
+     * Logs when an interest message is received
+     * @param hostID local peer, the one who was given interest
+     * @param peerID peer that is interested
+     */
+    public static void logInterest(int hostID, int peerID){
+        System.out.format("[%s]: Peer [%d] received the 'interested' message from [%d]%n", LocalDateTime.now().toString(), hostID, peerID);
+    }
+
+
+    /**
+     * Logs when a not interest message is received
+     * @param hostID local peer, the one who was given not interest message
+     * @param peerID peer that is not interested
+     */
+    public static void logNotInterest(int hostID, int peerID){
+        System.out.format("[%s]: Peer [%d] received the 'not interested' message from [%d]%n", LocalDateTime.now().toString(), hostID, peerID);
+    }
+
+    /**
+     * logs when a have message is recieved
+     * @param hostID peer recieving have
+     * @param peerID peer sending have
+     */
+    public static void logHave(int hostID, int peerID) {
+        System.out.format("[%s]: Peer [%d] received the 'have' message from [%d]%n", LocalDateTime.now().toString(), hostID, peerID);
+    }
+
+
+    /**
+     * Logs whenever local host downloads peice from neighbor
+     * @param hostID id of local peer
+     * @param peerID id of neighbor peer
+     * @param pieceIndex index of peice being downloaded
+     */
+    public static void logDownload(int hostID, int peerID, int pieceIndex){
+        System.out.format("[%s]: Peer [%d] has downloaded the piece [%d] from [%d]%n", LocalDateTime.now().toString(), hostID, pieceIndex, peerID);
+    }
+
+    /**
+     * Logs when file download is complete
+     * @param hostID local peer
+     */
+    public static void logComplete(int hostID){
+        System.out.format("[%s]: Peer [%d] has downloaded the complete file%n", LocalDateTime.now().toString(), hostID);
     }
 }
